af8b9431970dee5ab927c83a3dd9ea281e00344a
Martin Alfke <ma@example42.com>
2017-05-17 13:59:47 +0200

    multi os single tests

--- spec/classes/frankfurt-workshop_single_spec.rb
+++ spec/classes/frankfurt-workshop_single_spec.rb
@@ -1,7 +1,16 @@
 # spec/classes/fankfurt-workshop_spec.rb
 require 'spec_helper'
-describe 'frankfurt-workshop', :type => :class do
-  context 'it should compile' do
+describe 'frankfurt_workshop', :type => :class do
+  context 'it should compile on RedHat family' do
+    let(:facts) {{
+      :osfamily => 'RedHat',
+    }}
+    it { should compile.with_all_deps }
+  end
+  context 'it should compile on Debian family' do
+    let(:facts) {{
+      :osfamily => 'Debian',
+    }}
     it { should compile.with_all_deps }
   end
 end
